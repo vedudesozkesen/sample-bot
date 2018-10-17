d45b25ed33dbc2a5040ac680417dbdd57da08c97
İmran Durmuş <35728572+5humrud@users.noreply.github.com>
2018-10-17 20:19:11 +0300

    Add files via upload

--- Robot.java
+++ Robot.java
@@ -7,112 +7,161 @@
 
 package org.usfirst.frc.team2905.robot;
 
-import com.sun.prism.paint.Color;
+import edu.wpi.first.wpilibj.AnalogGyro;
 
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Relay;
-import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
-public class Robot extends IterativeRobot {
-
-	int internalCounter = 0;
-	//RobotDrive myArm;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.usfirst.frc.team2905.robot.commands.ExampleCommand;
+import org.usfirst.frc.team2905.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 
-	VictorSP frontLeft;
-	VictorSP rearLeft;
-	VictorSP frontRight;
-	VictorSP rearRight;
-	VictorSP armMechanismUpDownMotor;
-	VictorSP rightLiftMotor;
-	VictorSP leftLiftMotor;
-	RobotDrive myRobot;
-	DigitalOutput rightArmValve ;
-	DigitalOutput leftArmValve ;
-	Joystick stick;
-	Joystick gamepad;
+
+
+
+
+public class Robot extends TimedRobot {
+	public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
+	public static OI m_oi;
+
+	Command m_autonomousCommand;
+	SendableChooser<Command> m_chooser = new SendableChooser<>();
+	private SpeedControllerGroup left = new SpeedControllerGroup(new VictorSP(0), new VictorSP(1));
+	private SpeedControllerGroup right = new SpeedControllerGroup(new VictorSP(2), new VictorSP(3));
+	AnalogGyro gyro = new AnalogGyro(0);
+	DifferentialDrive  myRobot = new DifferentialDrive(right , left);
+	Joystick stick = new Joystick(0);
+	Joystick gamepad = new Joystick(1);
+	private Timer timer = new Timer();
+	//DigitalOutput rightArmValve = new DigitalOutput(0);
+	//DigitalOutput leftArmValve = new DigitalOutput(1);
+	//DoubleSolenoid rightArmValve = new DoubleSolenoid(0,1 );
+	//DoubleSolenoid leftArmValve = new DoubleSolenoid(2,3);
+	DigitalInput limitSwitchSlow;
+	DigitalInput limitSwitchSstop;
+	VictorSP armMechanismUpDownMotor = new VictorSP(4);
+	VictorSP rightLiftMotor = new VictorSP(5);
+	VictorSP leftLiftMotor = new VictorSP(6);	
 	JoystickButton button1;
 	JoystickButton button2;
-	int autoLoopCounter;
+	Alliance color  = DriverStation.getInstance().getAlliance();
+	double Kp = 0.03;
+	double leftSlow = 0.24;
+	double rightSlow = -0.24;
+	double rotateSpeed = 0.35;
+	double rotateSpeedSlow = 0.25;
+	limitSwitchSstop = new DigitalInput(1) ;
+	double myRobot;
+	
+	
 
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
+	
+	
+	
+	
+	@Override
 	public void robotInit() {
-		frontLeft = new VictorSP(7);
-		rearLeft = new VictorSP(6);
-		frontRight = new VictorSP(9);
-		rearRight = new VictorSP(8);
-		armMechanismUpDownMotor = new VictorSP(5);
-		rightLiftMotor = new VictorSP(4);
-		leftLiftMotor = new VictorSP(3);
-		rightArmValve = new DigitalOutput(0);
-		leftArmValve = new DigitalOutput(1);
-		myRobot = new RobotDrive(rearLeft, frontLeft, rearRight, frontRight);
-		//myRobot.setInvertedMotor(RobotDrive.frontLeft, true);
-		//myRobot.setInvertedMotor(RobotDrive.rearLeft, true);
+		m_oi = new OI();
+		m_chooser.addDefault("Default Auto", new ExampleCommand());
+		// chooser.addObject("My Auto", new MyAutoCommand());
+		SmartDashboard.putData("Auto mode", m_chooser);
+		gyro.reset();
 		
-		stick = new Joystick(0);
-		gamepad = new Joystick(1);
-		Alliance color;
-		color = DriverStation.getInstance().getAlliance();
+		
 		
 		
 	}
 
-	/**
-	 * This function is run once each time the robot enters autonomous mode
-	 */
-	public void autonomousInit() {
-		autoLoopCounter = 0;
-		
-		// Getting Team Color
-		Alliance color;
-		color = DriverStation.getInstance().getAlliance(); 
-		
-		if(color == DriverStation.Alliance.Blue)
-		{
-			int a = 1;
-		}
-		else
-		{
-			int aa=2;
-		}
-		//Getting Location Number
-		
+	
+	@Override
+	public void disabledInit() {
+
 	}
 
-	/**
-	 * This function is called periodically during autonomous
-	 */
+	@Override
+	public void disabledPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	
+	@Override
+	
+	
+	public void autonomousInit() 
+	{
+		m_autonomousCommand = m_chooser.getSelected();
+		timer.reset();
+		timer.start();
+		gyro.reset();
+		
+		/*
+		  String autoSelected = SmartDashboard.getString("Auto Selector", "Default");
+		  switch(autoSelected) 
+		  { case "My Auto": autonomousCommand
+		  = new MyAutoCommand(); break; case "Default Auto": default:
+		  autonomousCommand = new ExampleCommand(); break; }
+		  */
+		 
+		
+		// schedule the autonomous command (example)
+		if (m_autonomousCommand != null) 
+		{
+			m_autonomousCommand.start();
+		}
+	}
+
+	
+	@Override
 	public void autonomousPeriodic() {
+		Scheduler.getInstance().run();
+		int autoLoopCounter = 0;
+		
+		//boolean gyromsg = true;
 		
 		while(isAutonomous() && isEnabled())
 		{
+		myRobot (0.5,0.5);
+		}
 			
-			String gameData;
-			gameData = DriverStation.getInstance().getGameSpecificMessage();
+			/*if(Math.abs(gyro.getAngle()) <=3)
+			{
+				left.set(leftSlow - (gyro.getAngle())/15);
+				//right.set(right - gyro);
+			}/*
 			
-			if(gameData.length()>0)
+			
+			
+			
+			
+			
+			
+			//double angle = gyro.getAngle();
+			//double setangle = 1.0;
+			//myRobot.setExpiration(setangle);
+			//DriverStation.reportError("Gyro derece degeri" + angle, gyromsg);
+			
+			//String gameData = DriverStation.getInstance().getGameSpecificMessage();
+			
+			/*if(gameData.length()>0)
 			{
 				if(gameData.charAt(0) == 'L')
 				{
-					if(autoLoopCounter < 300) //Check if we've completed 100 loops (approximately 2 seconds)
+					if(autoLoopCounter < 1000) //Check if we've completed 100 loops (approximately 2 seconds)
 					{
 						leftLiftMotor.set(-0.5); 	// drive forwards half speed
 						autoLoopCounter++;
@@ -124,29 +173,63 @@ public class Robot extends IterativeRobot {
 				{
 					leftLiftMotor.set(0.0);
 				}
-			}
+			}*/
+			
 			
 			
 		}
 		
+	private void myRobot(double d, double e) {
+		// TODO Auto-generated method stub
+		
 	}
 
-	/**
-	 * This function is called once each time the robot enters tele-operated mode
-	 */
-	public void teleopInit(){
 	}
 
-	/**
-	 * This function is called periodically during operator control
-	 */
+	@Override
+	public void teleopInit() {
+		
+		
+		
+		if (m_autonomousCommand != null) {
+			m_autonomousCommand.cancel();
+		}
+	}
+
+	
+	@Override
 	public void teleopPeriodic() {
+		Scheduler.getInstance().run();
+		
 		
 		while(isOperatorControl() && isEnabled())
 		{
-			myRobot.arcadeDrive(stick);
+			if(limitSwitchSstop.get()) //Stopper robotun kolu belirli bir yere kadar kalktiginda switch tarafýndan algilandiginda kendisini durdurmasý icin.
+		    {
+		          rightLiftMotor.set(0.0);// kolu durdur
+		          leftLiftMotor.set(0.0);}
+			myRobot.arcadeDrive(stick.getY(),stick.getX());
+			boolean colormsg = true;
+			}
+					
+					
+					
+				
 			
-			//myRobot.arcadeDrive(0.5, 0.0);
+			
+			// Getting Team Color
+			/*Alliance color;
+			color = DriverStation.getInstance().getAlliance(); 
+			
+			if(color == DriverStation.Alliance.Blue)
+			{
+				DriverStation.reportWarning("Team color is Blue", colormsg);
+			}
+			else
+			{
+				DriverStation.reportWarning("Team color is Red", colormsg);
+			}*/
+			//Getting Location Number
 			
 			//***** Arm Mechanism Up/Down Motor - GamePad Button 3 and 4 *****//
 			
@@ -189,7 +272,7 @@ public class Robot extends IterativeRobot {
 					//***** Arm Open/Close - GamePad Button 5 and 6 *****//
 					
 					//Up
-					if(gamepad.getRawButton(5)== true && gamepad.getRawButton(6) == false)
+					/*if(gamepad.getRawButton(5)== true && gamepad.getRawButton(6) == false)
 					{
 						rightArmValve.set(true);
 						leftArmValve.set(true);
@@ -202,18 +285,14 @@ public class Robot extends IterativeRobot {
 						rightArmValve.set(false);
 						leftArmValve.set(false);
 					}
-		}
-		
-		
-		
-		
+		}*/
 	}
+		
+		
+	
 
-	/**
-	 * This function is called periodically during test mode
-	 */
+	
+	@Override
 	public void testPeriodic() {
-		LiveWindow.run();
-	}
-
+	}/*
 }
